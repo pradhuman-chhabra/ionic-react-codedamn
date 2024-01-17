6a08d0cc92eaad1abddab75ed7b9fc67e5ee8b6e
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2024-01-18 00:13:30 +0530

    added slide function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,24 @@
-import { IonContent, IonHeader, IonItem, IonList, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonList, IonPage, IonText, IonTitle, IonToolbar, IonLabel, IonAvatar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+
+
+const arr = [
+  {
+    name: 'Finn',
+    desc: 'This guy rocks!'
+  },
+  {
+    name: 'Ha',
+    desc: 'Trust me, I am a programmer'
+  },
+  {
+    name: 'Rey',
+    desc: 'I am done with it!'
+  }
+]
+
+
 
 const Home: React.FC = () => {
   return (
@@ -12,20 +30,27 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent className='ion-padding'>
         <IonList>
-          <IonItem>
-            <IonText>
-              {Array(10)
-                .fill(0)
-                .map((_, i) => (
-                  <IonItem key={i}>
-                    <IonText>List Item {i}</IonText>
-                  </IonItem>
-                ))}
-            </IonText>
-          </IonItem>
+          {arr.map(elem => (
+            <IonItemSliding key={elem.name}>
+              <IonItem>
+                <IonAvatar>
+                  <img
+                    src={`https://ionic framework.com/docs/demos/api/list/avatar-${elem.name.toLowerCase()}.png`}
+                  />
+                </IonAvatar>
+                <IonLabel className="ion-padding">
+                  <h2>{elem.name}</h2>
+                  <h3>{elem.desc}</h3>
+                  <p>Some helper text!</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="start">
+                <IonItemOption onClick={() => alert("pressed Delete")}>Delete</IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>))}
         </IonList>
       </IonContent>
-    </IonPage>
+    </IonPage >
   );
 };
 
