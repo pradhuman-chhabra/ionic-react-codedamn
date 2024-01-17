eff195009b27b0156e5c39b4d6c37b2ca9607d7e
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2024-01-17 23:16:54 +0530

    third commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonList, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader >
         <IonToolbar>
           <IonTitle>Hello world</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className='ion-padding'>
-        hello world
+        <IonList>
+          <IonItem>
+            <IonText>
+              {Array(10)
+                .fill(0)
+                .map((_, i) => (
+                  <IonItem key={i}>
+                    <IonText>List Item {i}</IonText>
+                  </IonItem>
+                ))}
+            </IonText>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
